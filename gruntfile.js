681d944d52a6b479c95dc87161767b977e0b2be7
Anthony Albertorio <anthony.albertorio@gmail.com>
2017-03-02 14:51:40 -0500

    eliminated grunt-contrib-clean and grunt-contrib-copy as dev dependencies from package.json.

--- gruntfile.js
+++ gruntfile.js
@@ -68,8 +68,6 @@ module.exports = function(grunt){
 	});	
 
 	// Load the plugins
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-watch');
